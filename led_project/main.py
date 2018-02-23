62910e7372248f24f6ea5730f5b9458a434f9406
Martin Casey <caseym3@tcd.ie>
2018-02-23 01:04:57 +0000

    Sketched out some basic tests for the main operations

--- led_project/main.py
+++ led_project/main.py
@@ -4,8 +4,20 @@ import numpy as np
 # going to try using numpy array with zeros and ones 
 # https://stackoverflow.com/questions/21174961/how-to-create-a-numpy-array-of-all-true-or-all-false
 # leds initialised to false/off
-L = 10
+
+input_file = 0 # placeholder for var for input file
+
+#with open(input_file) as f:
+    #for line in f:
+        #method here for processing each line
+        # look back at data analysis notes for open() file stuff.
+
+
+
+L = 10 # test var for size. given in the first line of the input file
 
 grid = np.full((L, L), False)
 
+
+print(grid.dtype)
 print(grid)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,9 +12,31 @@ class BasicTestSuite(unittest.TestCase):
     def testGridSize(self):
         assert(main.grid.size) == main.L ** 2
 
-# need to test 
-
-
+    def testInputFileExists(self):
+        assert(main.input_file) != None
+    
+    # should test out each of the three operations.
+    # although might not need separate methods for each one. 
+    # could just have a process input line method.
+    
+    #def testTurnOnFromOff(self):
+        #assert (main.turn_on()) == True
+    
+    #def testTurnOnWhenOn(self):
+        #assert (main.turn_on()) == True
+        
+    #def testTurnOffFromOn(self):
+        #assert (main.turn_off()) == False
+    
+    #def testTurnOffWhenOff(self):
+        #assert (main.turn_off()) == False
+        
+    #def testSwitchWhenOn(self):
+        #assert (main.switch()) == False
+        
+    #def testSwitchWhenOff(self):
+        #assert (main.switch()) == True
+    
 
 if __name__ == '__main__':
     unittest.main()
