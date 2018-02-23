00f01157dd597c06072acec210333a79c1a38e5a
Martin Casey <caseym3@tcd.ie>
2018-02-23 01:40:09 +0000

    Followed numpy tutorial and made some notes to code later.

--- led_project/main.py
+++ led_project/main.py
@@ -12,12 +12,22 @@ input_file = 0 # placeholder for var for input file
         #method here for processing each line
         # look back at data analysis notes for open() file stuff.
 
+#actually, setup.py 'should install a script which reads input from a file'
 
 
-L = 10 # test var for size. given in the first line of the input file
+L = 8 # test var for size. given in the first line of the input file
 
 grid = np.full((L, L), False)
 
+# two formats for selecting data from multi-d arr. double bracket and single bracket with comma.
+# single bracket recommended in tutorial. 
+# [:,:] rows, cols
+# therefore, can have (0,0 through 999,0) == [0:999, 0:1] or [0:999, 0]
+# i think i can do something like grid[0:999, 0:1] = True
+# can change multiple values using slice notation and = to assign
 
-print(grid.dtype)
+
+print(grid.dtype, '\n')
+print(grid, '\n')
+grid[0:8,0:1] = True
 print(grid)
