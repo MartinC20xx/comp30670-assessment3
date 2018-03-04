1e4ee87b7b1fd3122facb3dc6517bfb197d430f6
Martin Casey <caseym3@tcd.ie>
2018-03-04 16:42:49 +0000

    "Tidied up instructions processor methods"

--- led_project/instructions_processor.py
+++ led_project/instructions_processor.py
@@ -2,41 +2,36 @@
 Currently all methods relating to instruction processing. May need refactoring.
 '''
 
-#def get_size(self, instructions_list):
-   # pass
 
 
 
 def process_line(self, grid, parsed_line):
     
+    numbers = parsed_line[1:]
     # first test command is valid, if not, ignore line
     if(parsed_line[0] != 'turn on' and parsed_line[0] != 'turn off' and parsed_line[0] != 'switch'):
         pass # come back to this
     
     elif(parsed_line[0] == 'turn on'):
-        
-            numbers = parsed_line[1:]
-            grid_size = main.g
-        
             turn_on(numbers)
         
-    elif(parsed_inst[1] == 'turn off'):
-            turn_off(parsed_inst[2], parsed_inst[3])
+    elif(parsed_line[0] == 'turn off'):
+            turn_off(numbers)
             
-    elif(parsed_inst[1] == 'switch'):
-            turn_on(parsed_inst[2], parsed_inst[3])
+    elif(parsed_line[0] == 'switch'):
+            switch(numbers)
         
 
-def turn_on(self, numbers, grid_size):
-    
+def turn_on(self, numbers):
     
     pass
 
-def turn_off(self):
+def turn_off(self, numbers):
     pass
 
-def switch(self):
+def switch(self, numbers):
     pass
+
 
 def count_lights(self, grid):
     pass
